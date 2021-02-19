f29d75bd526dd607191ca1b4c9c83aff4d864e3d
Robin Mailhot <robin.mailhot@usherbrooke.ca>
2021-02-19 17:05:50 -0500

    created main

---
+++ main.py
@@ -0,0 +1,25 @@
+from Computer_Vision.inference_pipeline import *
+import cv2
+import time
+
+def init_sequence():
+    camera = cv2.VideoCapture(0)
+    return camera
+
+
+def main():
+    model_path = "Computer_Vision/models/yolov4-tiny-416-fp16-nutz.tflite"
+
+    camera = init_sequence() # WILL NEED TO PUT THE REST OF THE INIT IN IT (LOL)
+    n=0
+    start = time.time()
+    while True:
+        box_list = inference(camera,model_path)
+    camera.release()
+    return 1
+
+if __name__ == '__main__':
+    try:
+        main()
+    except SystemExit:
+        pass
