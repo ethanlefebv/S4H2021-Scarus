d7b13972b59d81add8267fd0b2981bc9822ea541
HaQcer <Charles.Caya@USherbrooke.ca>
2021-04-01 10:54:29 -0400

    Flushed inference_log()

--- Computer_Vision/inference_pipeline.py
+++ Computer_Vision/inference_pipeline.py
@@ -12,8 +12,12 @@ def coord_cam_to_robot(nut):
     return nut
 
 
-def get_inference_nut_log(cam, model_path):
-    nuts_list = timed_inference(cam, model_path)
+def get_inference_nut(cam, model_path, should_log):
+    if should_log:
+        nuts_list = timed_inference(cam, model_path)
+    else:
+        nuts_list = inference(cam, model_path)
+
     if nuts_list == []:
         nuts_list = [[0, 0, INVALID_NUT]]
 
@@ -31,19 +35,6 @@ def timed_inference(input_image, model_path):
     inf_time = time.time() - start_time
     print("Time spent in inference : {0}".format(inf_time))
     return nuts_list
-
-
-def get_inference_nut(cam, model_path):
-    nuts_list = inference(cam, model_path)
-    if nuts_list == []:
-        nuts_list = [[0, 0, INVALID_NUT]]
-
-    first_nut = nuts_list[0]
-    x = first_nut[0]
-    y = first_nut[1]
-    nut_class = int(first_nut[2])
-    print('Yolo model outputs : ', x, y, nut_class)
-    return first_nut
 
 
 def inference(input_image, model_path):
